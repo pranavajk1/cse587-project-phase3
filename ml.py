2ecd51a56f58158ed036a9e19323ee5708b83811
anupbana <anupbana@buffalo.edu>
2023-12-09 18:51:30 -0500

    errors rectified

--- ml.py
+++ ml.py
@@ -42,23 +42,6 @@ def preprocess(sentiment_data: pd.DataFrame) -> pd.DataFrame:
 
   return sentiment_data['text']
 
-def preprocess_text(text):
-  stop_words = set(stopwords.words('english'))
-  stemmer = PorterStemmer()
-  words = word_tokenize(text.lower())
-  filtered_words = [stemmer.stem(word) for word in words if word.isalnum() and word not in stop_words]
-  return ' '.join(filtered_words)
-
-def preprocess_text_nn(text):
-  max_words = 10000  # Maximum number of words to keep in the vocabulary
-  max_len = 280  # Maximum sequence length
-
-  tokenizer = Tokenizer(num_words=max_words)
-  tokenizer.fit_on_texts(text)
-  sequences = tokenizer.texts_to_sequences(text)
-  padded_sequences = pad_sequences(sequences, maxlen=max_len, padding='post', truncating='post')
-  return padded_sequences
-
 
 ann_model = tf.keras.models.load_model('./models/ann_model.h5')
 rnn_model = tf.keras.models.load_model('./models/rnn_model.h5')
@@ -68,24 +51,65 @@ multinomial_nb = joblib.load('./models/multinomial_naive_bayes_model.pkl')
 logistic_regression = joblib.load('./models/logistic_regression_model.pkl')
 linear_svm = joblib.load('./models/linear_svm_model.pkl')
 
+def preprocess_text_nn(test_comments):
+  max_words = 10000  # Maximum number of words to keep in the vocabulary
+  max_len = 280  # Maximum sequence length
+  tokenizer = Tokenizer(num_words=max_words)
+  test_sequence = tokenizer.texts_to_sequences([test_comments])
+  padded_test_sequence = pad_sequences(test_sequence, maxlen=max_len, padding='post', truncating='post')
+  return padded_test_sequence
+
+def nn_model_final_output(text, predictions):
+  sentiment_mapping = {
+    'negative': 0,
+    'neutral': 1,
+    'positive': 2
+  }
+  predicted_label_index = predictions.argmax()
+  predicted_label = list(sentiment_mapping.keys())[list(sentiment_mapping.values()).index(predicted_label_index)]
+  print(f"\nComment: {text}")
+  print(f"Predicted sentiment: {predicted_label}")
+  return predicted_label
+
+
+def preprocess_text(text):
+  # Preprocessing: Convert text to lowercase, remove stopwords, and perform stemming
+  stop_words = set(stopwords.words('english'))
+  stemmer = PorterStemmer()
+
+  def preprocess_text(text):
+      words = word_tokenize(text.lower())
+      filtered_words = [stemmer.stem(word) for word in words if word.isalnum() and word not in stop_words]
+      return ' '.join(filtered_words)
+  
+  # Load the CountVectorizer used during training
+  vectorizer_loaded = joblib.load('models/vectorizer.pkl')
+  user_input_processed = preprocess_text(text)
+  user_input_vectorized = vectorizer_loaded.transform([user_input_processed])
+  return(user_input_vectorized)[0]
+
 def predict_text(text, model):
   if model not in ['Multinomial Naive Bayes', 'Logistic', 'Linear SVM']:
     preprocessed_text = preprocess_text_nn(text)
     if model == 'ANN':
-      prediction =  ann_model.predict(preprocessed_text)
+      predictions =  ann_model.predict(preprocessed_text)[0]
     elif model == 'RNN':
-      return rnn_model.predict(preprocessed_text)
+      predictions = rnn_model.predict(preprocessed_text)[0]
     elif model == 'LSTM':
-      return lstm_model.predict(preprocessed_text)
+      predictions =  lstm_model.predict(preprocessed_text)[0]
+    final_output = nn_model_final_output(text, predictions)
+
   else:
     preprocessed_text = preprocess_text(text)
-    vectorizer = CountVectorizer()
-    vectorizer.fit(preprocessed_text)
-    vector = vectorizer.transform(preprocessed_text)
     if model == 'Multinomial Naive Bayes':
-      prediction =  multinomial_nb.predict(vector)
+      prediction = multinomial_nb.predict(preprocessed_text)
     elif model == 'Logistic':
-      prediction =  logistic_regression.predict(vector)
+      prediction =  logistic_regression.predict(preprocessed_text)
     elif model == 'Linear SVM':
-      prediction =  linear_svm.predict(vector)
-    return prediction
+      prediction =  linear_svm.predict(preprocessed_text)
+    # Convert predicted label index to a string label
+    final_output = 'Neutral' if prediction == 1 else ('Negative' if prediction == 0 else 'Positive')
+    print(f"\nComment: {text}")
+    print(f"Predicted sentiment: {final_output}")
+
+  return final_output
